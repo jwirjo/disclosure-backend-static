c44242435121365f34080c33a6b3c08bd44f75e7
Aaron D Borden <adborden@a14n.net>
2016-10-08 23:13:52 -0700

    Update total_loans_received_calculator

--- calculators/total_loans_received_calculator.rb
+++ calculators/total_loans_received_calculator.rb
@@ -1,14 +1,16 @@
 class TotalLoansReceivedCalculator
   def initialize(candidates: [], ballot_measures: [], committees: [])
     @candidates_by_filer_id =
-      candidates.where('"FPPC" IS NOT NULL').index_by { |c| c.FPPC }
+      candidates.where('"FPPC" IS NOT NULL').index_by { |c| c.FPPC.to_s }
+    @candidate_committees_by_netfile_id =
+      committees.where("\"FilerLocalId\" IS NOT NULL AND \"FilerStateId\" IN ('#{@candidates_by_filer_id.keys.join "','"}')").index_by { |c| c.FilerLocalId }
   end
 
   def fetch
     @results = ActiveRecord::Base.connection.execute(<<-SQL)
       SELECT "FilerLocalId", SUM("Amount_A") AS "Amount_A"
       FROM "efile_COAK_2016_Summary"
-      WHERE "FilerLocalId" IN ('#{@candidates_by_filer_id.keys.join "', '"}')
+      WHERE "FilerLocalId" IN ('#{@candidate_committees_by_netfile_id.keys.join "', '"}')
       AND "Form_Type" = 'F460'
       AND "Line_Item" = '2'
       GROUP BY "FilerLocalId"
@@ -16,7 +18,8 @@ class TotalLoansReceivedCalculator
     SQL
 
     @results.each do |row|
-      candidate = @candidates_by_filer_id[row['FilerLocalId'].to_i]
+      committee = @candidate_committees_by_netfile_id[row['FilerLocalId']]
+      candidate = @candidates_by_filer_id[committee.FilerStateId]
       candidate.save_calculation(:total_loans_received, row['Amount_A'].to_f)
     end
   end
